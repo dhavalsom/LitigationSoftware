787b339ec382079c3edab8364d39927d574a9ec5
Dhaval R Somaiya <dhavalsom@gmail.com>
2018-07-19 23:56:44 +0530

    some previous minor changes

---
+++ Database/Queries/insert_[ITSectionMaster]-actual.sql
@@ -0,0 +1,27 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+insert into [LitigationApp].[dbo].[ITSectionMaster]
+      ([Description]
+      ,[IsDefault]
+      ,[Active]
+      ,[AddedBy]
+      ,[AddedDate]
+      ,[IsReturn])
+values('139(1)',1,1,1,GETUTCDATE(),1)
+
+insert into [LitigationApp].[dbo].[ITSectionMaster]
+      ([Description]
+      ,[IsDefault]
+      ,[Active]
+      ,[AddedBy]
+      ,[AddedDate]
+      ,[IsReturn])
+values('139(4)',0,1,1,GETUTCDATE(),1)
+
+insert into [LitigationApp].[dbo].[ITSectionMaster]
+      ([Description]
+      ,[IsDefault]
+      ,[Active]
+      ,[AddedBy]
+      ,[AddedDate]
+      ,[IsReturn])
+values('143(1)',0,1,1,GETUTCDATE(),1)
