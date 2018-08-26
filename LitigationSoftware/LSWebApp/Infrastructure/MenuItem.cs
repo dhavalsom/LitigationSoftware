66d1985f38caea2cc9505b2a9cadf30d8e247c33
Kunal <kunalsmehtajobs@gmail.com>
2018-08-26 21:06:05 +0530

    Implemented dynamic menu (read from xml file)

---
+++ LitigationSoftware/LSWebApp/Infrastructure/MenuItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LSWebApp.Infrastructure
+{
+    public class MenuItem
+    {
+        public int Id { get; set; }
+        public string ControllerName { get; set; }
+        public string ActionName { get; set; }
+        public string DisplayText { get; set; }
+        public bool HasChildren
+        {
+            get
+            {
+                return SubMenu != null && SubMenu.Count() > 0;
+            }
+        }
+        public List<MenuItem> SubMenu { get; set; }
+    }
+}
