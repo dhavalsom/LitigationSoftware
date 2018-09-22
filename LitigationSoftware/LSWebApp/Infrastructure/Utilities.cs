500e2f155deef92c5fd294201c099bdbb3e6b4ec
Kunal <kunalsmehtajobs@gmail.com>
2018-09-22 17:12:58 +0530

    Fixed number formatting and negative numbers brackets issue

---
+++ LitigationSoftware/LSWebApp/Infrastructure/Utilities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+namespace LSWebApp.Infrastructure
+{
+    public class Utilities
+    {
+        public static string FormatNumber(decimal? number)
+        {
+            if (number.HasValue)
+            {
+                return string.Format("{0:C}", number).Replace("$", string.Empty);
+            }
+            return string.Empty;
+        }
+    }
+}
