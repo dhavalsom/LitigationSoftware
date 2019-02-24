6d9707be9ac375820570285feb50dca948c32329
Dhaval R Somaiya <dhavalsom@gmail.com>
2019-02-24 22:17:15 +0530

    Input sheet UI changes

---
+++ Database/Dhaval/InsertData/update_itheadmaster_uichanges.sql
@@ -0,0 +1,38 @@
+update [LitigationApp].dbo.[ITHeadMaster] set ExcelSrNo = 4 where PropertyName = 'IncomefromSalary'
+update [LitigationApp].dbo.[ITHeadMaster] set ExcelSrNo = 5,Description='Income from House Property' where PropertyName = 'HousePropIncome'
+update [LitigationApp].dbo.[ITHeadMaster] set ExcelSrNo = 6 where PropertyName = 'IncomefromBusinessProf'
+update [LitigationApp].dbo.[ITHeadMaster] set ExcelSrNo = 7 where PropertyName = 'IncomefromSpeculativeBusiness'
+update [LitigationApp].dbo.[ITHeadMaster] set ExcelSrNo = 8,Description='Income from Capital Gains(LTCG)' where PropertyName = 'IncomefromCapGainsLTCG'
+update [LitigationApp].dbo.[ITHeadMaster] set ExcelSrNo = 9,Description='Income from Capital Gains(STCG)' where PropertyName = 'IncomefromCapGainsSTCG'
+update [LitigationApp].dbo.[ITHeadMaster] set ExcelSrNo = 10,Description='Income from Other Sources' where PropertyName = 'IncomeFromOtherSources'
+update [LitigationApp].dbo.[ITHeadMaster] set ExcelSrNo = 11,Description='Current year Losses adjustment/ Brought forward losses/ carry forward losses' where PropertyName = 'Broughtforwardlosses'
+update [LitigationApp].dbo.[ITHeadMaster] set ExcelSrNo = 12,Description='Deductions under Chapter VIA & any other deduction (eg. 10A/ 10AA /10B/ 80 HHC, etc.)' where PropertyName = 'DeductChapterVIA'
+update [LitigationApp].dbo.[ITHeadMaster] set ExcelSrNo = 13,Description='Book Profit as per MAT/ AMT' where PropertyName = 'ProfitUS115JB'
+update [LitigationApp].dbo.[ITHeadMaster] set ExcelSrNo = 14,Description='Advance Tax (1st Installment)' where PropertyName = 'AdvanceTax1installment'
+update [LitigationApp].dbo.[ITHeadMaster] set ExcelSrNo = 15,Description='Advance Tax (2nd Installment)' where PropertyName = 'AdvanceTax2installment'
+update [LitigationApp].dbo.[ITHeadMaster] set ExcelSrNo = 16,Description='Advance Tax (3rd Installment)' where PropertyName = 'AdvanceTax3installment'
+update [LitigationApp].dbo.[ITHeadMaster] set ExcelSrNo = 17,Description='Advance Tax (4th Installment)' where PropertyName = 'AdvanceTax4installment'
+update [LitigationApp].dbo.[ITHeadMaster] set ExcelSrNo = 18 where PropertyName = 'TDS'
+update [LitigationApp].dbo.[ITHeadMaster] set ExcelSrNo = 19 where PropertyName = 'TDS26AS'
+update [LitigationApp].dbo.[ITHeadMaster] set ExcelSrNo = 20 where PropertyName = 'TDSasperBooks'
+update [LitigationApp].dbo.[ITHeadMaster] set ExcelSrNo = 21 where PropertyName = 'TCSPaidbyCompany'
+update [LitigationApp].dbo.[ITHeadMaster] set ExcelSrNo = 22 where PropertyName = 'TaxCollectedAtSource'
+update [LitigationApp].dbo.[ITHeadMaster] set ExcelSrNo = 23 where PropertyName = 'SelfAssessmentTax'
+update [LitigationApp].dbo.[ITHeadMaster] set ExcelSrNo = 24 where PropertyName = 'ForeignTaxCredit'
+update [LitigationApp].dbo.[ITHeadMaster] set ExcelSrNo = 25 where PropertyName = 'MATCredit'
+update [LitigationApp].dbo.[ITHeadMaster] set ExcelSrNo = 26 where PropertyName = 'InterestUS234A'
+update [LitigationApp].dbo.[ITHeadMaster] set ExcelSrNo = 27 where PropertyName = 'InterestUS234B'
+update [LitigationApp].dbo.[ITHeadMaster] set ExcelSrNo = 28 where PropertyName = 'InterestUS234C'
+update [LitigationApp].dbo.[ITHeadMaster] set ExcelSrNo = 29 where PropertyName = 'InterestUS234D'
+update [LitigationApp].dbo.[ITHeadMaster] set ExcelSrNo = 30 where PropertyName = 'InterestUS220'
+update [LitigationApp].dbo.[ITHeadMaster] set ExcelSrNo = 31 where PropertyName = 'RefundAdjusted'
+update [LitigationApp].dbo.[ITHeadMaster] set ExcelSrNo = 32 where PropertyName = 'RegularAssessment'
+update [LitigationApp].dbo.[ITHeadMaster] set ExcelSrNo = 33 where PropertyName = 'RefundAlreadyReceived'
+
+                               
+                
+                
+                
+                
+                
+                
